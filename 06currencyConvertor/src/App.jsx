6c0b326b11fb64b30701a21d3b74ef6e932c4e7c
Vivek sherwal <viveksherwal@gmail.com>
2025-11-15 23:29:54 +0530

    system hi  khede

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'  
+import React from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-500'> Currency Convertor</h1>
+    </>
+  )
+}
+
+export default App
