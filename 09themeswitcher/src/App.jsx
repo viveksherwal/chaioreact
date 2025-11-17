3ecc10a7becea2137b0c2c9a04b3248ec3109bec
Vivek sherwal <viveksherwal@gmail.com>
2025-11-18 01:27:59 +0530

    toggletheme

---
+++ 09themeswitcher/src/App.jsx
@@ -0,0 +1,48 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
+import React from 'react'
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // actual change in theme
+useEffect(() => {
+  if (themeMode === "dark") {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+}, [themeMode]);
+
+  
+
+  return (
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
