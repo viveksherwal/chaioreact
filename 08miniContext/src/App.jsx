6c4fe6b3db61cfb67cdcc1c26170d69b7f8562b6
Vivek sherwal <viveksherwal@gmail.com>
2025-11-17 22:53:34 +0530

    context api

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
+import React from 'react'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
